002ee60d5f798d5564ae472090cdec87fea70eb4
anthony-robinson <acrobinson794@gmail.com>
2020-06-13 03:05:26 -0700

    added chat bubble prototype

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 function App() {
 	return (
-		<div className="flex flex-col min-h-screen h-screen">
-			<div className="flex text-white font-light">
+		<div className="flex-1 flex flex-col min-h-screen h-screen">
+			<div className="flex text-white font-sans font-light">
 				<div className="bg-gray-800 w-56 flex-none flex items-center justify-between px-3 py-2">
-					<div>Flack</div>
+					<div className="font-bold">Flack</div>
 				</div>
 				<div className="flex-1 bg-gray-700 flex items-center justify-between px-4 border-b border-gray-900">
 					<div className="flex items-center">
@@ -25,25 +25,38 @@ function App() {
 			</div>
 
 			<div className="flex-1 bg-blue-500 flex overflow-y-hidden">
-				<div className="bg-gray-700 w-56 flex-none overflow-y-auto">
-					Hashtag lorem ipsum big lots of words paragraphs overflowing Lorem ipsum dolor sit amet, consectetur
-					adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-					veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-					irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-					Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-					est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-					incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-					ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-					voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet,
-					consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-					enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-					nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-					mollit anim id est laborum.
+				<div className="bg-gray-700 w-56 flex-none flex flex-col">
+					<div className="text-sm overflow-y-auto">
+						<ul className="px-2 py-3">
+							<li className="text-gray-500 px-2 hover:text-gray-200 hover:bg-gray-900">
+								<a href="#" class="flex items-center">
+									<span class="text-xl">#</span>
+									<span class="ml-2">welcome</span>
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 				<div className="bg-green-500 flex-1 flex justify-between">
-					<div className="bg-gray-600 flex-1 overflow-y-auto">Display chat</div>
+					<div className="bg-gray-600 flex-1 overflow-y-auto">
+						<div className="text-sm text-gray-400 overflow-y-auto">
+							<div className="flex mx-6 my-3 py-4 border-t border-gray-700 ">
+								<div className="ml-5">
+									<div>
+										<a href="#" className="text-white hover:underline">
+											Anthony
+										</a>
+										<span className="text-xs text-gray-600 ml-1">07/19/2020</span>
+									</div>
+									<div>
+										<div>yeah haha</div>
+										<div>wyd today? wanna go hiking?</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
 					<div className="bg-indigo-600 w-56 flex-none overflow-y-auto">Display chat 2</div>
 				</div>
 			</div>
