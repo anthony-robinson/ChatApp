d1cc1a5e295d7697e14e399f7562b37f4fcf38b0
anthony-robinson <acrobinson794@gmail.com>
2020-06-14 20:22:14 -0700

    added hooks to update state

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 
-const ChatInput = () => {
+const ChatInput = ({ inputText }) => {
+	const [ text, setTextValue ] = useState('');
+	const onSubmit = (e) => {
+		e.preventDefault();
+		inputText(text);
+		console.log(text);
+	};
 	return (
-		<form className="bg-gray-700 h-8 flex justify-end items-center border-t border-gray-600 " id="chatinput">
+		<form
+			onSubmit={onSubmit}
+			className="bg-gray-700 h-8 flex justify-end items-center border-t border-gray-600 "
+			id="chatinput"
+		>
 			<div className="flex-1">
 				<input
+					onChange={(e) => setTextValue(e.target.value)}
 					type="text"
 					className="w-full font-light rounded bg-gray-700 text-gray-200 text-xs px-2 py-1 focus:outline-none"
-					type="text"
 					placeholder="Enter a message..."
 				/>
 			</div>
