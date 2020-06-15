f28184a4403fd61afa05f28a306a337a35534157
anthony-robinson <acrobinson794@gmail.com>
2020-06-14 20:27:31 -0700

    accepts props from parent

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const MyMessage = () => {
+const MyMessage = ({ userMessage }) => {
 	return (
 		<div className="flex justify-end mx-6 my-3 py-4 border-gray-700 ">
 			<div className="mr-5">
@@ -10,8 +10,8 @@ const MyMessage = () => {
 					</a>
 					<span className="text-xs text-gray-600 ml-1">07/19/2020</span>
 				</div>
-				<div class="max-w-sm mx-auto flex px-3 py-1 bg-blue-500 rounded-lg shadow-xl">
-					<div class="text-sm text-white font-light">Hey wyd today?</div>
+				<div class="flex px-3 py-1 bg-blue-500 rounded-lg shadow-xl">
+					<div class="text-sm text-white font-light">{userMessage}</div>
 				</div>
 			</div>
 		</div>
