45acbc4ba8f19219ad140c23822e1cf568cc4a95
anthony-robinson <acrobinson794@gmail.com>
2020-06-16 18:59:27 -0700

    changed styling

--- src/components/FriendMessage.js
+++ src/components/FriendMessage.js
@@ -8,7 +8,7 @@ const FriendMessage = ({ userMessage }) => {
 					<a href="#" className="text-white hover:underline">
 						MyFriend
 					</a>
-					<span className="text-xs text-gray-600 ml-1">07/19/2020</span>
+					<span className="text-xs text-green-600 ml-1">07/19/2020</span>
 				</div>
 				<div class="max-w-sm mx-auto inline-block break-words px-3 py-1 bg-gray-100 rounded-lg shadow-xl ">
 					<div class="text-sm text-black font-light">{userMessage}</div>
