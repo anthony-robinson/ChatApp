dbf834f79af203ffa4bb9545b41b0e3e75906821
anthony-robinson <acrobinson794@gmail.com>
2020-06-30 23:58:06 -0700

    changed styling

--- src/components/MyMessage.js
+++ src/components/MyMessage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import FlipMove from 'react-flip-move';
 const MyMessage = ({ userMessage }) => {
 	return (
 		<div className="flex justify-end mx-3 my-3 py-4 border-gray-700 ">
@@ -8,7 +8,7 @@ const MyMessage = ({ userMessage }) => {
 					<a href="#" className="text-white hover:underline">
 						Anthony
 					</a>
-					<span className="text-xs text-green-600 ml-1">07/19/2020</span>
+					<span className="text-xs text-black ml-1">06/29/2020</span>
 				</div>
 				<div class="inline-block break-words max-w-sm px-3 py-1 bg-blue-500 rounded-lg shadow-xl">
 					<div class="text-sm text-white font-light">{userMessage}</div>
